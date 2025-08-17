ef2dd84724fc7ac8d3b3bd78a4282c9eb4d467eb
muh-younus <braq369@gmail.com>
2025-08-17 04:23:41 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express")
+const path = require("path");
+
+const app  = express();
+
+//setup static folder path
+
+app.use(express.static(path.join(__dirname, "Frontend")))
+
+const post= [
+{id: "1", title: "test1", type:"check"},
+{id: "2", title: "test2", type:"check"},
+{id: "3", title: "test3", type:"check"},
+{id: "4", title: "test4", type:"check"}
+
+]
+
+app.get('/api',(req,res) =>{
+
+    res.json(post)
+})
+
+
+app.listen(8000, ()=> console.log("Server is running on port 8000"));
